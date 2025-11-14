b03f0c2f4480b37a6b0b6744ad62627a0299f3d2
Junaidasif975 <junaidasif250@gmail.com>
2025-11-14 18:39:12 +0500

    Hero Section

---
+++ app/Data Structure/Banner.ts
@@ -0,0 +1,27 @@
+interface bannerType{
+    title:string;
+    description:string;
+    button:string;
+}
+export const banner:bannerType[]=[
+    {
+        title:"100% Original Parts",
+        description:"We use only OEM parts for replacement",
+        button:"Learn More"
+    },
+    {
+        title:"Dealership Quality Repair",
+        description:"Latest equipment and our expert technicians are trained on each brand",
+        button:"Enquire Now"
+    },
+    {
+        title:"Service Contract & Warranty Plans",
+        description:"All your car services and warranty needs taken care of",
+        button:"Book Now"
+    },
+    {
+        title:"Pick Up Across UAE",
+        description:"We come to you no matter where you are in the UAE",
+        button:"Schedule Now"
+    }
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
 import Component1 from "./components/Component1";
 import Offers from "./components/Offers";
-
+import Banner from "./components/Banner";
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Banner />
     <Component1 />
     <Offers />
     </div>
