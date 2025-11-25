906eebaa8554b121f89b085358ff4293f0a38764
Vr7Rizwan <ma9342341@gmail.com>
2025-11-25 15:04:05 +0500

    change styling in why choose us section

--- app/components/WhyChooseUS.tsx
+++ app/components/WhyChooseUS.tsx
@@ -6,19 +6,18 @@ function WhyChooseUS({
   whyChooseUs: whyChooseUsTypes[] | undefined;
 }) {
   return (
-    <div
-      className="bg-cover relative bg-center bg-no-repeat min-h-screen flex justify-center"
-      style={{ backgroundImage: "url('/assets/background.png')" }}
-    >
-      <div className="absolute inset-0 bg-black/70"></div>
+    <div className="bg-primary bg-cover relative bg-center bg-no-repeat min-h-screen flex justify-center">
       <div className="w-full z-10 py-15 flex items-center flex-col gap-15">
-        <h2 className="text-primary font-bold subHeading">WHY CHOOSE <span className="text-secondary font-bold subHeading">US</span></h2>
+        <h2 className="text-txtColor font-bold subHeading">
+          WHY CHOOSE{" "}
+          <span className="text-secondary font-bold subHeading">US</span>
+        </h2>
         <div className="flex flex-col w-full gap-5 justify-around px-5">
           {whyChooseUs &&
             whyChooseUs.map((item, index) => (
               <div
                 key={index}
-                className="bg-primary px-10 py-10 flex flex-col gap-5 rounded-2xl text-primary"
+                className="px-10 py-10 flex flex-col gap-5 rounded-2xl border-2 border-secondary hover:bg-secondary hover:border-txtColor hover:cursor-pointer transition-all duration-500"
               >
                 <h3 className="text-txtColor subHeading font-bold">
                   {item.title}
