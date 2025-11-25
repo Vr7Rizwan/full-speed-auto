91ad269f06fef86bc4e22ed1c64de03cf6d04760
Vr7Rizwan <ma9342341@gmail.com>
2025-11-25 15:43:36 +0500

    change name from brandServices to services

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,20 +1,26 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-const BrandServices = ({ Services }: { Services: {name:string;link:string;description:string;}[] | undefined }) => {
-    const [flip, setFlip] = useState<number | null>(null);
-    return (
-        <section className="w-full py-16 px-6 md:px-12 lg:px-20 bg-gray-50">
-            {Services && (
-                <div>
-                    <div className="text-center mb-12">
-                        <h1 className="heading md:text-4xl font-bold text-txtColor mb-3">
-                           Automotive Solutions
-                        </h1>
-                        <p className="text-txtColor max-w-5xl mx-auto text-base md:text-lg">
-                          Expert care for maintenance, repairs, and performance upgrades, ensuring your vehicle runs smoothly, stays reliable, and delivers its full potential.
-                        </p>
-                    </div>
+const Services = ({
+  Services,
+}: {
+  Services: { name: string; link: string; description: string }[] | undefined;
+}) => {
+  const [flip, setFlip] = useState<number | null>(null);
+  return (
+    <section className="w-full py-16 px-6 md:px-12 lg:px-20 bg-gray-50">
+      {Services && (
+        <div>
+          <div className="text-center mb-12">
+            <h1 className="heading md:text-4xl font-bold text-txtColor mb-3">
+              Automotive Solutions
+            </h1>
+            <p className="text-txtColor max-w-5xl mx-auto text-base md:text-lg">
+              Expert care for maintenance, repairs, and performance upgrades,
+              ensuring your vehicle runs smoothly, stays reliable, and delivers
+              its full potential.
+            </p>
+          </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {Services.map((value, i) => (
@@ -65,4 +71,4 @@ const BrandServices = ({ Services }: { Services: {name:string;link:string;descri
   );
 };
 
-export default BrandServices;
+export default Services;
