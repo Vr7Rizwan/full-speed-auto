e1f0e5ec2c453a7e5f1d2062fc709ad0fa4dced9
abdulrehmanafzal <146198964+abdulrehmanafzal@users.noreply.github.com>
2025-11-26 13:48:51 +0500

    About banner fixed

--- app/components/About/AboutBanner.tsx
+++ app/components/About/AboutBanner.tsx
@@ -1,15 +1,14 @@
-const AboutBanner=()=>{
-    return(
-        <section className="relative">
-             <div className="w-full overflow-hidden">
-  <img
-    src="./assets/aboutbanner.webp"
-    alt="Hero"
-    className="w-full max-h-[400px] object-cover"
-  />
-</div>
-            <div className="absolute inset-0 bg-black/35"></div>
-        </section>
-    )
+const AboutBanner = () => {
+  return (
+    <section className="">
+      <div className="w-full overflow-hidden">
+        <img
+          src="./assets/aboutbanner.webp"
+          alt="Hero"
+          className="w-full max-h-[400px] object-cover"
+        />
+      </div>
+    </section>
+  )
 }
 export default AboutBanner
