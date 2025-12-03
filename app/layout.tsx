659c469ea2c97e291ff431ee92103f556245e04d
abdulrehmanafzal <146198964+abdulrehmanafzal@users.noreply.github.com>
2025-12-03 14:44:58 +0500

    whatsapp button added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,8 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        {/* WhatsApp icon always visible */}
+        <WhatsAppButton />
         <Footer />
       </body>
     </html>
