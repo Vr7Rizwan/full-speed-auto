2e66339ac40699395eee6304469aa3f0b5366544
Vr7Rizwan <ma9342341@gmail.com>
2025-12-08 14:45:38 +0500

    update card width in about page

--- app/components/About/Cards.tsx
+++ app/components/About/Cards.tsx
@@ -6,7 +6,7 @@ const Cards = () => {
         {cards.map((value, i) => (
           <div
             key={`${value} ${i}`}
-            className="flex flex-col justify-center items-start w-full md:w-1/3 lg:w-1/4 md:h-130 lg:h-115 xl:h-95 gap-3 lg:gap-5 md:gap-10 px-6 py-6 md:px-8 md:py-6 border-secondary border rounded-2xl h-full"
+            className="flex flex-col justify-center items-start w-full md:w-4/10 lg:w-1/4 md:h-130 lg:h-115 xl:h-95 gap-3 lg:gap-5 md:gap-10 px-6 py-6 md:px-8 md:py-6 border-secondary border rounded-2xl h-full"
           >
             <img src={value.icon} alt="Error" className="w-20 invert" />
             <h1 className="subHeading font-medium text-primary">
