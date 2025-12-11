16facea3dc419422aac4e8be0d9fba7c031fff4a
abdulrehmanafzal <146198964+abdulrehmanafzal@users.noreply.github.com>
2025-12-11 18:48:33 +0500

    reduce gap in why choose us section

--- app/components/WhyChooseUS.tsx
+++ app/components/WhyChooseUS.tsx
@@ -17,7 +17,7 @@ function WhyChooseUS({
             whyChooseUs.map((item, index) => (
               <div
                 key={index}
-                className="p-5 flex flex-col gap-5 rounded-2xl border-2 border-secondary hover:bg-secondary hover:border-txtColor hover:cursor-pointer transition-all duration-500"
+                className="p-5 flex flex-col gap-2 rounded-2xl border-2 border-secondary hover:bg-secondary hover:border-txtColor hover:cursor-pointer transition-all duration-500"
               >
                 <h3 className="text-txtColor subHeading font-bold">
                   {item.title}
